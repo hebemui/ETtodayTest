b45ebe6b52478a819e9b91fcd63575b1eb73b67a
Hebe Mei <hoiian.m289@gmail.com>
2024-07-12 13:51:05 +0800

    Initialized a new project and removed SceneDelegate.

---
+++ EttodayTest/EttodayTest/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  EttodayTest
+//
+//  Created by Hebe Mui on 2024/7/12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ EttodayTest/EttodayTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EttodayTest
+//
+//  Created by Hebe Mui on 2024/7/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
